46ab3b240b228f3f8add5ac7d55d3e5181e4d09f
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-10-26 15:39:36 +0200

    new changes

--- src/api/routes/films.js
+++ src/api/routes/films.js
@@ -1,6 +1,5 @@
 const { isAuth, isAdmin } = require("../../middlewares/auth");
-// Importamos la función para crear el middleware dinámico
-const createUploadMiddleware = require("../../middlewares/file");
+const upload = require("../../middlewares/file");
 const {
   getFilms,
   getFilmsById,
@@ -14,30 +13,13 @@ const {
 
 const filmsRouter = require("express").Router();
 
-// Rutas para obtener películas
 filmsRouter.get("/", [isAdmin], getFilms);
 filmsRouter.get("/:id", getFilmsById);
 filmsRouter.get("/genre/:genre", getFilmsByGenre);
 filmsRouter.get("/year/:year", getFilmsByYear);
 filmsRouter.get("/runningTime/:runningTime", gettingFilmsByRunningTime);
-
-// Ruta para crear una película (sube la imagen a la carpeta "Films")
-filmsRouter.post(
-  "/",
-  [isAuth],
-  createUploadMiddleware("Films").single("image"), // Cambiamos el middleware a la carpeta "Films"
-  postFilm
-);
-
-// Ruta para actualizar una película (sube la imagen a la carpeta "UpdatedFilms")
-filmsRouter.put(
-  "/:id",
-  [isAdmin],
-  createUploadMiddleware("UpdatedFilms").single("image"), // Cambiamos el middleware a la carpeta "UpdatedFilms"
-  updateFilm
-);
-
-// Ruta para eliminar una película
+filmsRouter.post("/", [isAuth], upload.single("image"), postFilm);
+filmsRouter.put("/:id", [isAdmin], upload.single("image"), updateFilm);
 filmsRouter.delete("/:id", [isAdmin], deleteFilm);
 
 module.exports = filmsRouter;
